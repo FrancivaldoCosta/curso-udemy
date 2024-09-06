4dc03a24e9f7f90ffbb0cf721c221b420e2bd17f
FrancivaldoCosta <francivaldoaccosta@gmail.com>
2024-09-06 01:00:08 -0300

    Continua aula 219 pra 220

--- buffered_reader/src/application/Program.java
+++ buffered_reader/src/application/Program.java
@@ -1,31 +1,37 @@
 package application;
 
-import java.io.BufferedWriter;
-import java.io.FileWriter;
-import java.io.IOException;
+import java.io.File;
+import java.util.Scanner;
 
 public class Program {
 
 	public static void main(String[] args) {
+	
+		Scanner sc = new Scanner(System.in);
 		
-		String[] lines = new String[] { "Good morning", "Good afternoon", "Good night"};
+		System.out.println("Enter a folder path: ");
+		String strPath = sc.nextLine();
 		
-		String path = "c:\\temp\\out.txt";
+		File path = new File(strPath);
 		
-		try (BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
-			for (String line : lines) {
-				bw.write(line);
-				bw.newLine();
-				
-			}
-			
-			
+		File[] folders = path.listFiles(File::isDirectory);
+		System.out.println("FOLDERES:");
+		for (File folder : folders) {
+			System.out.println(folder);
 		}
-		catch (IOException e) {
-			 e.printStackTrace();
-		 }
 		
-
+		File[] files = path.listFiles(File::isFile);
+		System.out.println("FILES:");
+		for (File file : files) {
+			System.out.println(file);
+		}
+		
+		boolean success = new File(strPath + "\\subdir").mkdir();
+		System.out.println("Directory created successfully: " + success);
+		
+		
+		
+		sc.close();
 	}
 
 }
