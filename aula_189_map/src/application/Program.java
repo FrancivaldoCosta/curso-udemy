78f460344fcb24a1899d721cbc816a192c5eb429
Francivaldo Costa <francivaldoaccosta@gmail.com>
2024-10-24 16:50:10 -0300

    Aula 198 Function

--- aula_189_map/src/application/Program.java
+++ aula_189_map/src/application/Program.java
@@ -2,12 +2,20 @@ package application;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import entities.Product;
+import util.UpperCaseName;
 
 public class Program {
 
 	public static void main(String[] args) {
+		
+		Locale.setDefault(Locale.US);
+		Scanner sc = new Scanner(System.in);
 
 		List<Product> list = new ArrayList<>();
 
@@ -15,11 +23,18 @@ public class Program {
 		list.add(new Product("Mouse", 50.00));
 		list.add(new Product("Tablet", 350.50));
 		list.add(new Product("HD Case", 80.90));
-
-		double factor = 1.1;
 		
-		list.forEach(p -> p.setPrice(p.getPrice() * factor));
+		 
 
-		list.forEach(System.out::println);
+		List<String> names = list.stream().map(p -> p.getName().toUpperCase()).collect(Collectors.toList()); //AQUI O FUNC É PASSADO COMO ARGUMENTO 
+		
+		names.forEach(System.out::println);
+		
+		
+		
+		
+		
+		
+		sc.close();
 	}
 }
